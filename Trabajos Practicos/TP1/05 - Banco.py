d24c9d9bf58dadf50143de8dcbfa317be2146a8e
Xander.- <zn.xander@gmail.com>
2023-09-14 21:45:02 -0300

    Fix Ejercicio 5

--- Trabajos Practicos/TP1/05 - Banco.py
+++ Trabajos Practicos/TP1/05 - Banco.py
@@ -2,11 +2,11 @@ class Banco:
     def __init__(self, nombre, saldoTotal, tazasInteres):
         self.nombre = nombre
         self.saldoTotal = saldoTotal
-        self.tazasInteres = tazasInteres
+        self.tazasInteres = tazasInteres / 100 + 1
         self.clientes = []
     
     def abrirCuenta(self, nombre, apellido, dni, saldoActual = 0):
-        cliente = Cliente(nombre, apellido, dni, saldoActual)
+        cliente = Cliente(nombre, apellido, dni, saldoActual * self.tazasInteres)
         self.clientes.append(cliente)
         self.saldoTotal += saldoActual
 
@@ -18,7 +18,9 @@ class Cliente:
         self.saldoActual = saldoActual
     
     def __str__(self):
-        return f"Cliente: {self.apellido}, {self.nombre} (DNI: {self.dni}) - Saldo Actual: ${self.saldoActual}"
+        return f"Cliente: {self.apellido}, {self.nombre} (DNI: {self.dni}) - Saldo Actual: ${self.saldoActual:.2f}"
     
-b = Banco("Santander", 0, 1.1)
-b.abrirCuenta("Gabriel", "Romero", 33123456, 100)
+santander = Banco("Santander", 0, 10)
+santander.abrirCuenta("Gabriel", "Romero", 33123456, 100)
+for c in santander.clientes:
+    print(c)
